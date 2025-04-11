5eedc278524ce77058d5d1ae1a4475806e591487
nicknick75 <160857318+nicknick75@users.noreply.github.com>
2025-04-11 20:32:02 +0200

    comment change

--- src/main.jl
+++ src/main.jl
@@ -3,9 +3,9 @@ using Images, FileIO, ColorTypes, StaticArrays
 
 #------------najbolj osnovne stvari
 
-#osnovni tipi: ray
 
-#osnovne funkcije, ki jih rabiva: normaliziranje vektorjev, dot product? - mogoce bolj smiselno v utils.jl
+
+#osnovne funkcije: linear algebra paket
 
 #render funkcija: da dejansko generira sliko 
 #trenutno placeholder
