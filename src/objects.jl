5a2ea325166296b81d06d025dcdda673c1118768
LuckaaP <lp76800@student.uni-lj.si>
2025-04-11 19:40:05 +0200

    ravnina in sfera.2 prej ni nekaj delal

--- src/objects.jl
+++ src/objects.jl
@@ -2,18 +2,26 @@
 #sfera, ravnina, torus
 #ravnina
 struct Plane
-    f::Function
+    a::Float64
+    b::Float64
+    c::Float64
+    d::Float64
+    F::Function
     J::Function
 end
 
 function Plane(a, b, c, d)
     F(X) = a*X[1] + b*X[2] + c*X[3] - d
     J(X) = [a, b, c]
-    return Plane(F, J)
+    return Plane(a, b, c, d, F, J)
 end
 
 struct Sphere
-    f::Function
+    a::Float64
+    b::Float64
+    c::Float64
+    r::Float64
+    F::Function
     J::Function
 end
 
@@ -21,7 +29,7 @@ end
 function Sphere(a, b, c, r)
     F(X) = (X[1] - a)^2 + (X[2] - b)^2 + (X[3] - c)^2 - r^2
     J(X) = [2*X[1], 2*X[2], 2*X[3]]
-    return Sphere(F, J)
+    return Sphere(a, b, c, r, F, J)
 end
 
 # struct Torus
