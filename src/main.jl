a0a87011bb34721cdcfc770c9cfacc5679f31692
nicknick75 <nika.kralj27@gmail.com>
2025-04-11 16:33:55 +0200

    Rendering test

--- src/main.jl
+++ src/main.jl
@@ -8,5 +8,23 @@ using Images, FileIO, ColorTypes
 #osnovne funkcije, ki jih rabiva: normaliziranje vektorjev, dot product? - mogoce bolj smiselno v utils.jl
 
 #render funkcija: da dejansko generira sliko 
+#trenutno placeholder
+function render(sirina, visina) 
+    img = Array{RGB{N0f8}}(undef, sirina, visina)
 
-#entry point (kak je main v julii?)
+    for y in 1:sirina, x in 1:visina
+        img[y, x] = RGB{N0f8}(0.3, 0.5, 0.3)
+    end
+
+    return img
+end    
+
+
+function main()  
+    sirina, visina = 400, 400
+    img = render(sirina, visina)
+    save("../images/render.png", img)
+end
+
+
+
