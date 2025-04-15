dc86a3bb828fd0b4e35a02f9d1b7b617e920ccda
LuckaaP <lp76800@student.uni-lj.si>
2025-04-15 11:06:46 +0200

    ni še dokončan ampak črno belo če odkomentiraš neki dela

---
+++ src/test_pathtracer.jl
@@ -0,0 +1,73 @@
+using Images, FileIO, ColorTypes
+
+include("camera.jl")
+include("objects.jl")
+include("utils_pathtracer.jl")
+
+function render(sirina, visina)
+    p = [0.0, 0.0, 0.0]            
+    look_at = [0.0, 0.0, 5.0]      
+    up = [0.0, 1.0, 0.0]
+
+    w = normalize(look_at .- p)
+    x = normalize(cross(w, up))
+    y = normalize(cross(x, w))
+
+    aspect_ratio = sirina / visina
+    image_height = 2.0
+    image_width = aspect_ratio * image_height
+
+    left = -image_width / 2
+    right = image_width / 2
+    bottom = -image_height / 2
+    top = image_height / 2
+    d = 1.0
+
+    cam = Camera(p, x, y, w, left, right, bottom, top, d, sirina, visina)
+    # cam = Camera(
+    #     p, x, y, w,
+    #     -image_width/2, image_width/2,
+    #     -image_height/2, image_height/2,
+    #     1.0, sirina, visina
+    # )
+
+    #light_source = [1.0, 0.5, 0.0]
+    light = Sphere(1.0, 2.0, -1.5, 1.5, 0.0, RGB{N0f8}(0,0,0);light_source=true)
+
+    pink = RGB{N0f8}(1.0, 0.5, 0.8)
+    dark_gray = RGB{N0f8}(0.2, 0.2, 0.2)
+    light_gray = RGB{N0f8}(0.85, 0.85, 0.85)
+    light_blue = RGB{N0f8}(0.50, 0.91, 0.96)
+    olive = RGB{N0f8}(0.37, 0.56, 0.32)
+   
+    red_sphere   = Sphere(0.0, 0.0 , 2.0, 1.0, 32.0, RGB{N0f8}(1.0, 0.0, 0.0))   
+    #blue_sphere = Sphere(-0.8, 1.0, 1.3, 0.2, 64.0,RGB{N0f8}(0.0, 0.0, 1.0))   
+    #torus = Torus_side(0.3, 0.1, [-0.5, 0.0, 1.2], 32.0, pink)
+
+    #stene
+    floor   = Plane(1.0, 0.0, 0.0, -1.0, 0.0, olive)        
+    #ceiling = Plane(1.0, 0.0, 0.0, 2.0, 0.0, light_gray)       
+    #left    = Plane(0.0, 1.0, 0.0, 2.0, 0.0, light_gray)       
+    #right   = Plane(0.0, 1.0, 0.0, -2.0, 0.0, light_gray)      
+    back    = Plane(0.0, 0.0, 1.0, 5.0, 0.0, light_blue)     
+
+    ambient = 0.2
+    objects = [red_sphere, back, floor, light]
+    img = Array{RGB{N0f8}}(undef, cam.nx, cam.ny)
+
+    for j in 1:cam.ny, i in 1:cam.nx
+        ray = generate_ray(cam, i, j)
+        color = RGB{Float64}(0,0,0)
+        for k in 1:5
+            color += RGB{Float64}(pathtrace(ray, objects, 3, ambient, 0.5; max_inc=10))
+        end
+        img[i, j] = color / 5
+    end
+    return img
+end
+
+function main()
+   sirina, visina = 800, 800
+   img = render(sirina, visina)
+   save("../images/soba4.png", img)
+end
--- src/utils_pathtracer.jl
+++ src/utils_pathtracer.jl
@@ -2,7 +2,7 @@ using ColorTypes, LinearAlgebra
 
 include("utils.jl")  
 
-function pathtrace(ray, objects, light, depth, ambient, bounce_weight)
+function pathtrace(ray, objects, depth, ambient, bounce_weight; max_inc=50)
     if depth == 0
         return RGB{N0f8}(0.0, 0.0, 0.0)  # Base case: crna
     end
@@ -12,7 +12,7 @@ function pathtrace(ray, objects, light, depth, ambient, bounce_weight)
     rayfn(t) = ray.origin + t * ray.direction
     values = [sign(o.F(rayfn(t1))) for o in objects]
 
-    while t2 <= 50
+    while t2 <= max_inc
         t2 += 0.1
         r = rayfn(t2)
 
@@ -28,27 +28,42 @@ function pathtrace(ray, objects, light, depth, ambient, bounce_weight)
                 end
 
                 # to je direktna svetloba
-                light_dir = normalize(light .- hit_point)
-                in_shadow = v_senci(hit_point, light, objects)
-
+                # light_dir = normalize(light .- hit_point)
+                # in_shadow = v_senci(hit_point, light, objects)
+                if(objects[i].light_source)
+                    light =RGB{N0f8}(1,1,1) 
+                    return  #zabije v luč
+                end
                 base_color = RGB{Float64}(objects[i].color)
 
                 #če smo v senci - ambienta svetloba
-                if in_shadow
-                    direct_color = ambient * base_color
-                else
-                    direct_color = RGB{Float64}(lambert_shading(normal, light_dir, objects[i].color))
-                end
+                # if in_shadow
+                #     direct_color = ambient * base_color
+                # else
+                #     direct_color = RGB{Float64}(lambert_shading(normal, light_dir, objects[i].color))
+                # end
 
-                # nakljucna smer odbitega žarka (approx cosine-weighted)
+                # mogoče narediva več slik različnih random npr. noramlna porazdelitev monte carlo
+                # nakljucna smer odbitega žarka (approx cosine-weighted)  
                 rand_dir = normalize(normal + randn(3))
+                #@show rand_dir
                 new_ray = Ray(hit_point + 1e-4 * rand_dir, rand_dir)
 
-                indirect_color = RGB{Float64}(pathtrace(new_ray, objects, light, depth - 1, ambient, bounce_weight)
-)
+                # cos = clamp(dot(new_ray.direction,normal), 0.0, 1.0)
+                # if isnan(cos)
+                #     cos = 0.0
+                # end
+                # @show cos
 
-                final_color = direct_color + bounce_weight * indirect_color
-
+                indirect_color = RGB{Float64}(pathtrace(new_ray,objects, depth - 1, ambient, bounce_weight; max_inc=max_inc))
+                dist = norm(, )
+                
+                #ndirect_color = RGB{Float64}(pathtrace(new_ray, objects, light, depth - 1, ambient, bounce_weight; max_inc=max_inc))
+                #final_color = RGB{Float64}(base_color.r+indirect_color.r*cos/pi, base_color.g+indirect_color.g*cos/pi, base_color.b+indirect_color.b*cos/pi)
+                final_color = RGB{Float64}(base_color.r*indirect_color.r, base_color.g*indirect_color.g, base_color.b*indirect_color.b)
+                #final_color = direct_color + bounce_weight * indirect_color
+                
+                #return RGB{N0f8}(clamp01.(color)); #črno bela slika
                 return RGB{N0f8}(clamp01.(final_color))
             end
         end
