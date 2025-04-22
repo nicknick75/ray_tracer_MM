45e9976134a027094e8cb96f3f3bddc03a37a7a7
nicknick75 <160857318+nicknick75@users.noreply.github.com>
2025-04-23 00:48:25 +0200

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -1,67 +1,60 @@
-using Images, FileIO, ColorTypes, StaticArrays
+using Images, FileIO, ColorTypes, StaticArrays, LinearAlgebra
 
 include("camera.jl")
 include("objects.jl")
 include("utils.jl")
+include("ref_raf_utils.jl")
+function render_scene(sirina, visina)
+    p = [0.0, 0.0, -2.0]            
+    look_at = [0.0, 0.0, 3.0]      
+    up = [0.0, 1.0, 0.0]
 
-#------------najbolj osnovne stvari
+    w = normalize(look_at .- p)
+    x = normalize(cross(w, up))
+    y = normalize(cross(x, w))
 
+    aspect_ratio = sirina / visina
+    image_height = 2.0
+    image_width = aspect_ratio * image_height
 
+    left = -image_width / 2
+    right = image_width / 2
+    bottom = -image_height / 2
+    top = image_height / 2
+    d = 1.0
 
-#osnovne funkcije: linear algebra paket
+    cam = Camera(p, x, y, w, left, right, bottom, top, d, sirina, visina)
 
-#render funkcija: da dejansko generira sliko
-#trenutno placeholder
-function render(cam::Camera, objects, light_source)
+    light_source = [4.0, 3.0, -1.0]
+
+    # colors
+    yellow = RGB{N0f8}(1.0, 1.0, 0.0)
+    grey = RGB{N0f8}(0.6, 0.6, 0.6)
+    olive = RGB{N0f8}(0.37, 0.56, 0.32)
+    white = RGB{N0f8}(1.0, 1.0, 1.0)
+    blue = RGB{N0f8}(0.3, 0.6, 1.0)
+    red = RGB{N0f8}(1.0, 0.0, 0.0)
+
+    # objects
+    floor = Plane(1.0, 0.0, 0.0, -1.2, 0.0, olive, 1.0, false) 
+    solid = Sphere(0.5, 0.5, 5.0, 1.5, 0.0, red, 0, false) 
+
+    ambient = 0.3
+    objects = [floor, solid]
+
     img = Array{RGB{N0f8}}(undef, cam.nx, cam.ny)
 
     for j in 1:cam.ny, i in 1:cam.nx
         ray = generate_ray(cam, i, j)
-        color = raytrace(ray, objects, cam, light_source)
-        img[i, j] = color
+        img[i, j] = ref_raf_raytrace(ray, objects, cam, light_source, ambient; depth=3)
     end
-
     return img
 end
- 
-function look(perspective, target, up)
-    w = normalize(target .- perspective)
-    x = normalize(cross(up, w))
-    y = cross(w, x)
-    return x, y, w
-end
 
-
-function main()  
-    # Kamera setup
-    perspective = [0.0, 0.0, 0.0]
-    target = [0.0, 0.0, 5.0]
-    up = [0.0, 1.0, 0.0]
-    x, y, w = look(perspective, target, up)
-
-    cam = Camera(
-        perspective,
-        x, y, w,
-        -1.0, 1.0,     # left, right
-        -1.0, 1.0,     # bottom, top
-        1.0,           # focal length
-        400, 400       # resolution
-    )
-
-    #izvor svetolobe (light source)
-    light_source = [5.0, 5.0, -10.0]
-    
-    # Scena
-    sphere = Sphere(0.0, 1.0, 5.0, 2.0,RGB{N0f8}(1,0.329,0.686))
-    sphere2 = Sphere(0.0, 0.0, 3.0, 1.0,RGB{N0f8}(1,0.757,0.369))
-    plane = Plane(0.0, -0.5, 1.0, 5.0, RGB{N0f8}(0,0.77,0))
-    #plane2 = Plane( 0.0, -6.0, 0.20, 5 ,RGB{N0f8}(0,0,0.77))
-    objects = [sphere, sphere2, plane]
-
-    # Render
-    img = render(cam, objects, light_source)
-    save("../images/render.png", img)
-
+function main()
+    sirina, visina = 700, 700
+    img = render_scene(sirina, visina)
+    save("../images/scene.png", img)
 end
 
 
